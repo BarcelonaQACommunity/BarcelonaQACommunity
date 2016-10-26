ac73fa8c7454d0c284291205e34af72481df109e
NyoronDev <jsernajaen@gmail.com>
2016-10-26 22:08:56 +0200

    Add template.

--- src/ErniQACommunity/wwwroot/_references.js
+++ src/ErniQACommunity/wwwroot/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="js/freelancer.js" />
+/// <reference path="js/jqbootstrapvalidation.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
