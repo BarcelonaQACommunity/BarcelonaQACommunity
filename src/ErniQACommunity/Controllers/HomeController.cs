b9ba006605e95027b6094407b56dc27df4cb317d
NyoronDev <jsernajaen@gmail.com>
2017-01-20 20:20:51 +0100

    Update content

--- src/ErniQACommunity/Controllers/HomeController.cs
+++ src/ErniQACommunity/Controllers/HomeController.cs
@@ -6,13 +6,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ErniQACommunity.Controllers
 {
+    /// <summary>
+    /// HomeController class
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Errors this instance.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Error()
         {
             return View();
