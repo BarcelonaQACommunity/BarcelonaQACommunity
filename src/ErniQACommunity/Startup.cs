f86772b0f381a992aa1e6e1c65581b1f97d9e289
NyoronDev <nyoronsheppard@outlook.es>
2017-07-25 13:20:33 +0200

    Add development.

--- src/ErniQACommunity/Startup.cs
+++ src/ErniQACommunity/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace ErniQACommunity
+namespace BarcelonaQACommunity
 {
     public class Startup
     {
